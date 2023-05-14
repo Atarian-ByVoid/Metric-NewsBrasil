a171521a71ae9665805d6a1c483d775480aba3a4
Carlos Alberto <cgomespimentel7@gmail.com>
2023-05-14 09:35:55 -0300

    feat: Atualização das paginas de musica

--- JS/playerMusic.js
+++ JS/playerMusic.js
@@ -1,6 +1,8 @@
 const audioPlayer = document.querySelector('#audio-player');
 const playButton = document.querySelector('#play-button');
 const pauseButton = document.querySelector('#pause-button');
+const previousButton = document.querySelector('#previous-button');
+const nextButton = document.querySelector('#next-button');
 
 playButton.addEventListener('click', () => {
     audioPlayer.play();
@@ -8,4 +10,12 @@ playButton.addEventListener('click', () => {
 
 pauseButton.addEventListener('click', () => {
     audioPlayer.pause();
+});
+
+previousButton.addEventListener('click', () => {
+    audioPlayer.currentTime -= 10; // recua 10 segundos
+});
+
+nextButton.addEventListener('click', () => {
+    audioPlayer.currentTime += 10; // avança 10 segundos
 });
